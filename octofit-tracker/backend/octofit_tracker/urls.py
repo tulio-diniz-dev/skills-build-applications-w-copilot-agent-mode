3698aac1406d647a12433b8866ce7c800410d72f
tulio-diniz-dev <tulio.diniz@magalupay.com.br>
2025-11-10 19:21:07 +0000

    Refatora configuração de URLs para incluir namespace na API REST

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -2,21 +2,18 @@
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from .views import UserViewSet, TeamViewSet, ActivityViewSet, WorkoutViewSet, LeaderboardViewSet, api_root
+from .views import (
+    UserViewSet,
+    TeamViewSet,
+    ActivityViewSet,
+    WorkoutViewSet,
+    LeaderboardViewSet,
+)
+
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -25,29 +22,9 @@ router.register(r'activities', ActivityViewSet)
 router.register(r'workouts', WorkoutViewSet)
 router.register(r'leaderboard', LeaderboardViewSet)
 
-urlpatterns = [
-     path('admin/', admin.site.urls),
-     # Os endpoints da API REST seguem o padrão /api/[component]/
-     path('api/', include(router.urls)),
-]
-"""octofit_tracker URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Os endpoints da API REST seguem o padrão /api/[component]/
+    path('api/', include((router.urls, 'api'), namespace='api')),
 ]
